7b909cf2c739a829785e50f3dc5be4871d138ea2
Izzy <izzyinjurious@gmail.com>
2021-10-19 14:15:07 -0700

    exports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig)
+import config from './aws-exports';
+Amplify.configure(config);
 
 ReactDOM.render(<App />,document.getElementById('root'));
