f10d79c34697a8f63e00dadd514dacd45cdb2a73
Izzy <izzyinjurious@gmail.com>
2021-10-27 15:40:36 -0700

    removed lazy from footer

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,12 @@
 import React, {Suspense} from 'react';
 //components
 import Navbar from '../Navbar';
-// import Footer from '../Footer';
+import Footer from '../Footer';
 // import PersonalModal from '../PersonalModal';
 //stylesheets
 import './index.scss';
 
 const PersonalModal = React.lazy(() => import('../PersonalModal'));
-const Footer = React.lazy(() => import('../Footer'));
 const Layout = ({children}) => {
     return (
         <>
@@ -16,9 +15,7 @@ const Layout = ({children}) => {
             <Suspense fallback={<h1>Still Loading…</h1>}>
                 <PersonalModal/>
             </Suspense>  
-            <Suspense fallback={<h1>Still Loading…</h1>}>
                 <Footer />
-            </Suspense>
         </>
     )
 }
